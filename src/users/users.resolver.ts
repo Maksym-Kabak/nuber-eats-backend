2b313e508b33af7f4761b41e5e402e157ba8129b
maksym <maksitheslayr@gmail.com>
2021-06-12 12:13:48 +0300

    'finish Auth User'

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,19 +1,17 @@
-import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { Args, Context, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { User } from "./entities/user.entity";
 import { UsersService } from "./users.service";
 import { CreateAccountInput, CreateAccountOutput } from "./dtos/create-account.dto";
 import { LoginInput, LoginOutput } from "./dtos/login.dto";
+import { UseGuards } from "@nestjs/common";
+import { AuthGuard } from "../auth/auth.guard";
+import { AuthUser } from "../auth/auth-user.decorator";
 
 @Resolver(of => User)
 export class UsersResolver {
   constructor(
     private readonly usersService: UsersService
   ) {
-  }
-
-  @Query(returns => Boolean)
-  hi() {
-    return true;
   }
 
   @Mutation(returns => CreateAccountOutput)
@@ -35,5 +33,11 @@ export class UsersResolver {
         error
       };
     }
+  }
+
+  @Query(returns => User)
+  @UseGuards(AuthGuard)
+  me(@AuthUser() authUser: User) {
+    return authUser ;
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,11 +4,13 @@ import { Repository } from "typeorm";
 import { User } from "./entities/user.entity";
 import { CreateAccountInput } from "./dtos/create-account.dto";
 import { LoginInput } from "./dtos/login.dto";
+import { JwtService } from "../jwt/jwt.service";
 
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectRepository(User) private readonly users: Repository<User>
+    @InjectRepository(User) private readonly users: Repository<User>,
+    private readonly jwtService: JwtService
   ) {
   }
 
@@ -26,32 +28,37 @@ export class UsersService {
     }
   }
 
-  async login({ email, password }: LoginInput): Promise<{ ok: boolean, error?: string, token?: string}> {
+  async login({ email, password }: LoginInput): Promise<{ ok: boolean, error?: string, token?: string }> {
     try {
 
-      const user = await this.users.findOne({email})
-      if(!user) {
+      const user = await this.users.findOne({ email });
+      if (!user) {
         return {
           ok: false,
           error: "User not found"
-        }
+        };
       }
       const passwordCorrect = await user.checkPassword(password);
       if (!passwordCorrect) {
         return {
           ok: false,
           error: "Wrong password"
-        }
+        };
       }
 
+      const token = this.jwtService.sign(user.id);
       return {
         ok: true,
-        token: 'lalallala'
-      }
+        token
+      };
     } catch (error) {
       return {
         ok: false, error
       };
     }
+  }
+
+  async findById(id: number): Promise<User> {
+    return this.users.findOne({id})
   }
 }
