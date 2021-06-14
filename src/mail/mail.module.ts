aaf2aee4c1234a0acf1b57882ee5962afa0f4866
maksym <maksitheslayr@gmail.com>
2021-06-14 18:42:03 +0300

    'unit testing for UserService done'

---
+++ src/mail/mail.module.ts
@@ -0,0 +1,21 @@
+import { DynamicModule, Global, Module } from '@nestjs/common';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { MailModuleOption } from './mail.interfaces';
+import { MailService } from './mail.service';
+
+@Module({})
+@Global()
+export class MailModule {
+  static forRoot(options: MailModuleOption): DynamicModule {
+    return {
+      module: MailModule,
+      providers: [{
+        provide: CONFIG_OPTIONS,
+        useValue: options
+      },
+        MailService
+      ],
+      exports: [MailService]
+    };
+  }
+}
