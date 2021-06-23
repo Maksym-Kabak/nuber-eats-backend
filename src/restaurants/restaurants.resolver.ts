400d462139902bd6d4fb9ed496a0ae3478bb8735
Maksym-Kabak <maksitheslayer@gmail.com>
2021-06-23 16:57:15 +0300

    'done restaurants'

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
 import { CreateRestaurantInput, CreateRestaurantOutput } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entites/restaurant.entity';
 import { RestaurantsService } from './restaurants.service';
@@ -7,6 +7,12 @@ import { User } from '../users/entities/user.entity';
 import { Role } from '../auth/role.decorator';
 import { EditRestaurantInput, EditRestaurantOutput } from './dtos/edit-restaurant.dto';
 import { DeleteRestaurantInput, DeleteRestaurantOutput } from './dtos/delete-restaurant.dto';
+import { Category } from './entites/category.entity';
+import { AllCategoriesOutput } from './dtos/all-categories.dto';
+import { CategoryInput, CategoryOutput } from './dtos/category.dto';
+import { RestaurantsInput, RestaurantsOutput } from './dtos/restaurants.dto';
+import { RestaurantInput, RestaurantOutput } from './dtos/restaurant.dto';
+import { SearchRestaurantInput, SearchRestaurantOutput } from './dtos/search-restaurant.dto';
 
 @Resolver((of) => Restaurant)
 export class RestaurantsResolver {
@@ -38,5 +44,41 @@ export class RestaurantsResolver {
     @Args('input') deleteRestaurantInput: DeleteRestaurantInput,
   ): Promise<DeleteRestaurantOutput> {
     return this.restaurantService.deleteRestaurant(owner, deleteRestaurantInput);
+  }
+
+  @Query(returns => RestaurantsOutput)
+  restaurants(@Args('input') restaurantsInput: RestaurantsInput): Promise<RestaurantsOutput> {
+    return this.restaurantService.allRestaurants(restaurantsInput);
+  }
+
+  @Query(returns => RestaurantOutput)
+  restaurant(@Args('input') restaurantInput: RestaurantInput): Promise<RestaurantOutput> {
+    return this.restaurantService.findRestaurantBuId(restaurantInput);
+  }
+
+  @Query(returns => SearchRestaurantOutput)
+  searchRestaurant(@Args('input') searchRestaurantInput: SearchRestaurantInput): Promise<SearchRestaurantOutput> {
+    return this.restaurantService.searchRestaurantByName(searchRestaurantInput);
+  }
+}
+
+@Resolver(of => Category)
+export class CategoryResolver {
+  constructor(private readonly restaurantService: RestaurantsService) {
+  }
+
+  @ResolveField(type => Int)
+  restaurantCount(@Parent() category: Category): Promise<number> {
+    return this.restaurantService.countRestaurants(category);
+  }
+
+  @Query(type => AllCategoriesOutput)
+  allCategories(): Promise<AllCategoriesOutput> {
+    return this.restaurantService.allCategory();
+  }
+
+  @Query(type => CategoryOutput)
+  category(@Args('input') categoryInput: CategoryInput): Promise<CategoryOutput> {
+    return this.restaurantService.findCategoryBySlug(categoryInput);
   }
 }
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Like, Raw, Repository } from 'typeorm';
 import { Restaurant } from './entites/restaurant.entity';
 import { CreateRestaurantInput, CreateRestaurantOutput } from './dtos/create-restaurant.dto';
 import { User } from 'src/users/entities/user.entity';
@@ -8,6 +8,11 @@ import { Category } from './entites/category.entity';
 import { EditRestaurantInput, EditRestaurantOutput } from './dtos/edit-restaurant.dto';
 import { CategoryRepository } from './repositories/category.repository';
 import { DeleteRestaurantInput, DeleteRestaurantOutput } from './dtos/delete-restaurant.dto';
+import { AllCategoriesOutput } from './dtos/all-categories.dto';
+import { CategoryInput, CategoryOutput } from './dtos/category.dto';
+import { RestaurantsInput, RestaurantsOutput } from './dtos/restaurants.dto';
+import { RestaurantInput, RestaurantOutput } from './dtos/restaurant.dto';
+import { SearchRestaurantInput, SearchRestaurantOutput } from './dtos/search-restaurant.dto';
 
 @Injectable()
 export class RestaurantsService {
@@ -97,6 +102,115 @@ export class RestaurantsService {
       return {
         ok:false,
         error:'Could not delete restaurant',
+      };
+    }
+  }
+
+  async allCategory(): Promise<AllCategoriesOutput> {
+    try {
+      const categories = await this.categories.find();
+      return {
+        ok:true,
+        categories,
+      };
+    } catch (error) {
+      return {
+        ok:false,
+        error:'Cloud not load categories',
+      };
+    }
+  }
+
+  countRestaurants(category: Category) {
+    return this.restaurants.count({ category });
+  }
+
+  async findCategoryBySlug({ slug, page }: CategoryInput): Promise<CategoryOutput> {
+    try {
+      const category = await this.categories.findOne({ slug });
+      if (!category) {
+        return { ok:false, error:'Category not found' };
+      }
+      const restaurants = await this.restaurants.find({
+        where:{
+          category,
+        },
+        take:25,
+        skip:(page - 1) * 25,
+      });
+      category.restaurants = restaurants;
+      const totalResults = await this.countRestaurants(category);
+      return {
+        ok:true,
+        category,
+        restaurants,
+        totalPages:Math.ceil(totalResults / 25),
+      };
+    } catch (error) {
+      return {
+        ok:false,
+        error:'Cloud not load category',
+      };
+    }
+  }
+
+  async allRestaurants({ page }: RestaurantsInput): Promise<RestaurantsOutput> {
+    try {
+      const [restaurants, totalResults] = await this.restaurants.findAndCount({ skip:(page - 1) * 25, take:25 });
+      return {
+        ok:true,
+        results:restaurants,
+        totalPages:Math.ceil(totalResults / 25),
+        totalResults,
+      };
+    } catch (error) {
+      return {
+        ok:false,
+        error:'Could not load restaurants',
+      };
+    }
+  }
+
+  async findRestaurantBuId({ restaurantId }: RestaurantInput): Promise<RestaurantOutput> {
+    try {
+      const restaurant = await this.restaurants.findOne(restaurantId);
+      if (!restaurant) {
+        return {
+          ok:false,
+          error:'Restaurant not found',
+        };
+      }
+      return {
+        ok:true,
+        restaurant,
+      };
+    } catch (error) {
+      return {
+        ok:false,
+        error:'Could not load restaurant',
+      };
+    }
+  }
+
+  async searchRestaurantByName({ query, page }: SearchRestaurantInput): Promise<SearchRestaurantOutput> {
+    try {
+      const [restaurants, totalResults] = await this.restaurants.findAndCount({
+        where:{
+          name:Raw(name => `${name} ILIKE '%${query}%'`),
+        },
+        take:25,
+        skip:(page - 1) * 25,
+      });
+      return {
+        ok:true,
+        restaurants,
+        totalResults,
+        totalPages:Math.ceil(totalResults / 25),
+      };
+    } catch (error) {
+      return {
+        ok:false,
+        error:'Could not load restaurant',
       };
     }
   }
